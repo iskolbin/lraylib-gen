1b72abc8a76546b2b8ed537a149c18029761bba3
iskolbin <iskolbin@gmail.com>
2019-04-12 01:20:32 +0300

    upd

--- rail.lua
+++ rail.lua
@@ -16,16 +16,33 @@ local rail = {
 		},
 	},
 
+	time = raylib.GetTime,
+	frametime = raylib.GetFrameTime,
+	getfps = raylib.GetFPS,
+
 	window = {
 		getwidth = raylib.GetScreenWidth,
 		getheight = raylib.GetScreenHeight,
 		getsize = function() return raylib.GetScreenWidth(), raylib.GetScreenHeight() end,
+		settitle = raylib.SetWindowTitle,
+		setpos = raylib.SetWindowPosition,
+		setminsize = raylib.SetWindowMinSize,
+		setmonitor = raylib.SetWindowMonitor,
+		isminimized = raylib.IsWindowMinimized,
+		isready = raylib.IsWindowReady,
 	},
 
 	ctx = {
 		clear = raylib.ClearBackground,
 		text = raylib.DrawText,
-		pixel = raylib.DrawPixel,
+		points = function(...)
+			local n = select( '#', ... ) - 1
+			local color = select( n+1, ... )
+			for i = 1, n, 2 do
+				local x, y = select( i, ... )
+				raylib.DrawPixel( x, y, color )
+			end
+		end,
 		line = raylib.DrawLine,
 		beizer = raylib.DrawLineBezier,
 		strokerect = raylib.DrawRectangleLines,
@@ -37,7 +54,7 @@ local rail = {
 		fillcircle = raylib.DrawCircle,
 		gradcircle = raylib.CircleGradient,
 
-		texture = raylib.DrawTexture,
+		draw = raylib.DrawTexture,
 	},
 
 	gfx = {
@@ -69,9 +86,103 @@ local rail = {
 
 	draw = function() end,
 
+	mouse = {
+		setvisible = function( visible )
+			if visible then
+				raylib.ShowCursor()
+			else
+				raylib.HideCursor()
+			end
+		end,
+		isvisible = function()
+			return not raylib.IsCursorHidden()
+		end,
+		setenabled = function( enabled )
+			if enabled then
+				raylib.EnableCursor()
+			else
+				raylib.DisableCursor()
+			end
+		end,
+		ispressed = raylib.IsMouseButtonPressed,
+		isreleased = raylib.IsMouseButtonReleased,
+		isdown = raylib.IsMouseButtonDown,
+		isup = raylib.IsMouseButtonUp,
+		getx = raylib.GetMouseX,
+		gety = raylib.GetMouseY,
+		getpos = function() return raylib.GetMouseX(), raylib.GetMouseY() end,
+		setpos = raylib.SetMousePosition,
+		getwheel = raylib.GetMouseWheelMove,
+	},
+
 	keyboard = {
 		ispressed = raylib.IsKeyPressed,
+		isreleased = raylib.IsKeyReleased,
 		isdown = raylib.IsKeyDown,
+		isup = raylib.IsKeyUp,
+		getpressed = raylib.GetKeyPressed,
+	},
+
+	gamepad = {
+		isavailable = raylib.IsGamepadAvailable,
+		getname = raylib.GetGamepadName,
+		ispressed = raylib.IsGamepadButtonPressed,
+		isreleased = raylib.IsGamepadButtonReleased,
+		isdown = raylib.IsGamepadButtonDown,
+		isup = raylib.IsGamepadButtonUp,
+		getpressed = raylib.GetGamepadButtonPressed,
+		getaxis = raylib.GetGamepadAxisMovement,
+		getaxiscount = raylib.GetGamepadAxisCount,
+	},
+
+	touch = {
+		getx = raylib.GetTouchX,
+		gety = raylib.GetTouchY,
+		getpos = raylib.GetTouchPosition,
+		getcount = raylib.GetTouchPointsCount,
+	},
+
+	image = {
+		copy = raylib.ImageCopy,
+		textimage = function(...)
+			local n = select('#', ...)
+			if n == 3 then
+				raylib.ImageText(...)
+			elseif n == 5 then
+				raylib.ImageTextEx( ... )
+			end
+		end,
+		topow2 = raylib.ImageToPOT,
+		format = raylib.ImageFormat,
+		alphamask = raylib.ImageAlphaMask,
+		alhpaclear = raylib.ImageAlphaClear,
+		alphacrop = raylib.ImageAlphaCrop,
+		alphapremultiply = raylib.ImageAlphaPremultiply,
+		crop = raylib.ImageCrop,
+		resize = raylib.ImageResize,
+		resizenn = raylib.ImageResizeNN,
+		resizecanvas = raylib.ImageResizeCanvas,
+		mipmaps = raylib.ImageMipmaps,
+		dither = raylib.ImageDither,
+		rect = raylib.ImageDrawRectangle,
+		text = function(...)
+			local n = select('#', ...)
+			if n == 5 then
+				raylib.ImageDrawText(...)
+			elseif n == 7 then
+				raylib.ImageDrawTextEx( ... )
+			end
+		end,
+		vfilp = raylib.ImageFlipVertical,
+		hfilp = raylib.ImageFlipHorizontal,
+		rotcw = raylib.ImageRotateCW,
+		rotccw = raylib.ImageRotateCCW,
+		tint = raylib.ImageColorTint,
+		invert = raylib.ImageColorInvert,
+		grayscale = raylib.ImageColorGrayscale,
+		contrast = raylib.ImageColorContrast,
+		brightness = raylib.ImageColorBrightness,
+		replace = raylib.ImageColorReplace,
 	},
 }
 
@@ -85,7 +196,6 @@ local function updateImageMetatable()
 	local img = raylib.GenImageColor(0, 0, 0)
 	local mt = getmetatable( img )
 	mt.resize = function(...) raylib.ImageResize( ... ) end
-	print( mt, mt.resize )
 end
 
 function rail.run()
